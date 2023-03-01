125e44123ee35108684bd366035d1ec97a4a18bc
LizaSmirnov <122588135+LizaSmirnov@users.noreply.github.com>
2023-02-28 16:37:08 -0800

    first commit

---
+++ Assets/script.js
@@ -0,0 +1,30 @@
+
+
+
+
+//when i click a start button the timer starts
+//event lister
+//
+//
+// What is 12 X 12?
+// What is 48/6?
+// What is 100 x 55?
+// What is 60 x 88?
+
+
+var startButton = document.querySelector("#startButton");
+
+
+// Listen for a click event on toggle switch
+startButton.addEventListener("click", function() {
+    //button disappear
+    //quetions appear
+
+
+
+
+
+
+
+
+
